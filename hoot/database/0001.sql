5b7ead9961d60d69f5ef53b8b6c2cbd80ae7c59b
tlaukkan <tommi.s.e.laukkanen@gmail.com>
2013-09-30 17:52:43 +0300

    Added initial hoot sources.

---
+++ hoot/database/0001.sql
@@ -0,0 +1,222 @@
+CREATE DATABASE hoot WITH OWNER hoot;
+\connect hoot
+
+CREATE TABLE company (
+    companyid character varying(255) NOT NULL,
+    phonenumber character varying(255) NOT NULL,
+    invoicingemailaddress character varying(255) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    salesemailaddress character varying(255) NOT NULL,
+    companyname character varying(255) NOT NULL,
+    supportemailaddress character varying(255) NOT NULL,
+    companycode character varying(255) NOT NULL,
+    modified timestamp without time zone NOT NULL,
+    deliveryaddress_postaladdressid character varying(255),
+    invoicingaddress_postaladdressid character varying(255),
+    iban character varying(255) NOT NULL,
+    bic character varying(255) NOT NULL,
+    host character varying(255),
+    termsandconditions character varying(4096)
+);
+ALTER TABLE public.company OWNER TO hoot;
+
+CREATE TABLE customer (
+    customerid character varying(255) NOT NULL,
+    lastname character varying(255) NOT NULL,
+    phonenumber character varying(255) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    company boolean NOT NULL,
+    emailaddress character varying(255) NOT NULL,
+    firstname character varying(255) NOT NULL,
+    companyname character varying(255),
+    modified timestamp without time zone NOT NULL,
+    companycode character varying(255),
+    deliveryaddress_postaladdressid character varying(255),
+    invoicingaddress_postaladdressid character varying(255),
+    owner_companyid character varying(255) NOT NULL
+);
+ALTER TABLE public.customer OWNER TO hoot;
+
+CREATE TABLE group_ (
+    groupid character varying(255) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    description character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    modified timestamp without time zone NOT NULL,
+    owner_companyid character varying(255) NOT NULL
+);
+ALTER TABLE public.group_ OWNER TO hoot;
+
+CREATE TABLE groupmember (
+    groupmemberid character varying(255) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    group_groupid character varying(255),
+    user_userid character varying(255)
+);
+
+ALTER TABLE public.groupmember OWNER TO hoot;
+
+CREATE TABLE postaladdress (
+    postaladdressid character varying(255) NOT NULL,
+    addresslinetwo character varying(255),
+    addresslinethree character varying(255),
+    postalcode character varying(255),
+    addresslineone character varying(255),
+    city character varying(255),
+    country character varying(255)
+);
+ALTER TABLE public.postaladdress OWNER TO hoot;
+
+CREATE TABLE privilege (
+    privilegeid character varying(255) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    dataid character varying(255),
+    key character varying(255) NOT NULL,
+    user_userid character varying(255),
+    group_groupid character varying(255)
+);
+ALTER TABLE public.privilege OWNER TO hoot;
+
+CREATE TABLE user_ (
+    userid character varying(255) NOT NULL,
+    lastname character varying(255) NOT NULL,
+    phonenumber character varying(255) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    emailaddress character varying(255) NOT NULL,
+    firstname character varying(255) NOT NULL,
+    modified timestamp without time zone NOT NULL,
+    owner_companyid character varying(255) NOT NULL,
+    passwordhash character varying(256) NOT NULL
+);
+ALTER TABLE public.user_ OWNER TO hoot;
+
+INSERT INTO company VALUES ('3248528E-4D90-41F7-968F-AF255AD16901', '+358 40 1639099', 'invoice@bare.com', '2011-04-22 08:52:13.899', 'sales@bare.com', 'Test Company', 'support@bare.com', '0000000-0', '2011-04-22 08:52:13.899', '4EA7E643-3C80-49B2-8D1C-AAFA7E66A28C', 'CFE997C0-3FAF-4F6C-BBED-DB09689936B6', '-', '-', '127.0.0.1', '-');
+INSERT INTO group_ VALUES ('3DE5D850-B015-44C1-904C-86DC2B0276A4', '2012-02-13 21:37:24.804', 'Users', 'user', '2012-02-13 21:37:24.804', '3248528E-4D90-41F7-968F-AF255AD16901');
+INSERT INTO group_ VALUES ('1DE5D850-B015-44C1-904C-86DC2B0276A3', '2012-06-25 19:57:00', 'Administrators', 'administrator', '2012-06-25 19:57:00', '3248528E-4D90-41F7-968F-AF255AD16901');
+INSERT INTO groupmember VALUES ('50413BBB-DB86-402E-9E98-C7E73F219827', '2013-03-29 19:11:42.986', '1DE5D850-B015-44C1-904C-86DC2B0276A3', 'A591FCB8-772E-4157-B64B-4371A6C7CAE0');
+INSERT INTO postaladdress VALUES ('CFE997C0-3FAF-4F6C-BBED-DB09689936B6', '-', '-', '00000', '-', 'Helsinki', 'Finland');
+INSERT INTO postaladdress VALUES ('4EA7E643-3C80-49B2-8D1C-AAFA7E66A28C', '-', '-', '00000', '-', 'Helsinki', 'Finland');
+INSERT INTO user_ VALUES ('A591FCB8-772E-4157-B64B-4371A6C7CAE0', 'Test', '+123', '2013-03-29 18:21:23.769', 'admin@admin.org', 'User', '2013-03-29 18:21:23.769', '3248528E-4D90-41F7-968F-AF255AD16901', 'c8213c753f70e6ef82a3bbece671c183cc9aa70d944f2d8abbbc50ab7432f2b4');
+
+ALTER TABLE ONLY company
+    ADD CONSTRAINT company_pkey PRIMARY KEY (companyid);
+
+ALTER TABLE ONLY customer
+    ADD CONSTRAINT customer_pkey PRIMARY KEY (customerid);
+
+ALTER TABLE ONLY group_
+    ADD CONSTRAINT group__pkey PRIMARY KEY (groupid);
+
+ALTER TABLE ONLY groupmember
+    ADD CONSTRAINT groupmember_pkey PRIMARY KEY (groupmemberid);
+
+ALTER TABLE ONLY postaladdress
+    ADD CONSTRAINT postaladdress_pkey PRIMARY KEY (postaladdressid);
+
+ALTER TABLE ONLY privilege
+    ADD CONSTRAINT privilege_pkey PRIMARY KEY (privilegeid);
+
+ALTER TABLE ONLY group_
+    ADD CONSTRAINT unq_group__0 UNIQUE (owner_companyid, name);
+
+ALTER TABLE ONLY groupmember
+    ADD CONSTRAINT unq_groupmember_0 UNIQUE (user_userid, group_groupid);
+
+ALTER TABLE ONLY privilege
+    ADD CONSTRAINT unq_privilege_0 UNIQUE (user_userid, group_groupid);
+
+ALTER TABLE ONLY user_
+    ADD CONSTRAINT unq_user__0 UNIQUE (owner_companyid, emailaddress);
+
+ALTER TABLE ONLY user_
+    ADD CONSTRAINT user__pkey PRIMARY KEY (userid);
+
+ALTER TABLE ONLY company
+    ADD CONSTRAINT fk_company_deliveryaddress_postaladdressid FOREIGN KEY (deliveryaddress_postaladdressid) REFERENCES postaladdress(postaladdressid);
+
+ALTER TABLE ONLY company
+    ADD CONSTRAINT fk_company_invoicingaddress_postaladdressid FOREIGN KEY (invoicingaddress_postaladdressid) REFERENCES postaladdress(postaladdressid);
+
+ALTER TABLE ONLY customer
+    ADD CONSTRAINT fk_customer_billingaddress_postaladdressid FOREIGN KEY (invoicingaddress_postaladdressid) REFERENCES postaladdress(postaladdressid);
+
+ALTER TABLE ONLY customer
+    ADD CONSTRAINT fk_customer_deliveryaddress_postaladdressid FOREIGN KEY (deliveryaddress_postaladdressid) REFERENCES postaladdress(postaladdressid);
+
+ALTER TABLE ONLY customer
+    ADD CONSTRAINT fk_customer_owner_companyid FOREIGN KEY (owner_companyid) REFERENCES company(companyid);
+
+ALTER TABLE ONLY group_
+    ADD CONSTRAINT fk_group__owner_companyid FOREIGN KEY (owner_companyid) REFERENCES company(companyid);
+
+ALTER TABLE ONLY groupmember
+    ADD CONSTRAINT fk_groupmember_group_groupid FOREIGN KEY (group_groupid) REFERENCES group_(groupid);
+
+ALTER TABLE ONLY groupmember
+    ADD CONSTRAINT fk_groupmember_user_userid FOREIGN KEY (user_userid) REFERENCES user_(userid);
+
+ALTER TABLE ONLY privilege
+    ADD CONSTRAINT fk_privilege_group_groupid FOREIGN KEY (group_groupid) REFERENCES group_(groupid);
+
+ALTER TABLE ONLY privilege
+    ADD CONSTRAINT fk_privilege_user_userid FOREIGN KEY (user_userid) REFERENCES user_(userid);
+
+ALTER TABLE ONLY user_
+    ADD CONSTRAINT fk_user__owner_companyid FOREIGN KEY (owner_companyid) REFERENCES company(companyid);
+
+REVOKE ALL ON TABLE company FROM hoot;
+GRANT ALL ON TABLE company TO hoot;
+REVOKE ALL ON TABLE customer FROM hoot;
+GRANT ALL ON TABLE customer TO hoot;
+REVOKE ALL ON TABLE group_ FROM hoot;
+GRANT ALL ON TABLE group_ TO hoot;
+REVOKE ALL ON TABLE groupmember FROM hoot;
+GRANT ALL ON TABLE groupmember TO hoot;
+GRANT ALL ON TABLE groupmember TO hoot;
+REVOKE ALL ON TABLE postaladdress FROM hoot;
+GRANT ALL ON TABLE postaladdress TO hoot;
+REVOKE ALL ON TABLE user_ FROM hoot;
+GRANT ALL ON TABLE user_ TO hoot;
+
+CREATE TABLE schemaversion
+(
+  created timestamp without time zone NOT NULL,
+  schemaname character varying(255) NOT NULL,
+  schemaversion character varying(255) NOT NULL,
+  CONSTRAINT schemaversion_pkey PRIMARY KEY (created )
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE schemaversion
+  OWNER TO hoot;
+
+ALTER TABLE user_ ADD EMAILADDRESSVALIDATED BOOLEAN;
+UPDATE user_ SET EMAILADDRESSVALIDATED = true;
+ALTER TABLE user_ ALTER COLUMN emailaddressvalidated SET NOT NULL;
+
+CREATE TABLE entry
+(
+  entryid character varying(255) NOT NULL,
+  country character varying(2) NOT NULL,
+  created timestamp without time zone NOT NULL,
+  key character varying(1024) NOT NULL,
+  language character varying(2) NOT NULL,
+  modified timestamp without time zone NOT NULL,
+  value character varying(1024) NOT NULL,
+  owner_companyid character varying(255) NOT NULL,
+  basename character varying(1024) NOT NULL,
+  path character varying(2048) NOT NULL,
+  CONSTRAINT entry_pkey PRIMARY KEY (entryid ),
+  CONSTRAINT fk_entry_owner_companyid FOREIGN KEY (owner_companyid)
+      REFERENCES company (companyid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE entry
+  OWNER TO hoot;
+
+INSERT INTO schemaversion VALUES (NOW(), 'hoot', '0001');
+
---
+++ hoot/src/main/java/org/hoot/HootSynchronizer.java
@@ -0,0 +1,356 @@
+/**
+ * Copyright 2013 Tommi S.E. Laukkanen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hoot;
+
+import org.hoot.model.Entry;
+import org.apache.log4j.Logger;
+import org.vaadin.addons.sitekit.dao.CompanyDao;
+import org.vaadin.addons.sitekit.dao.UserDao;
+import org.vaadin.addons.sitekit.model.Company;
+import org.vaadin.addons.sitekit.model.Group;
+import org.vaadin.addons.sitekit.model.User;
+import org.vaadin.addons.sitekit.util.EmailUtil;
+import org.vaadin.addons.sitekit.util.PropertiesUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Class which synchronizes bundles to database and back.
+ *
+ * @author Tommi S.E. Laukkanen
+ */
+public class HootSynchronizer {
+    /** The logger. */
+    private static final Logger LOGGER = Logger.getLogger(HootSynchronizer.class);
+
+    /**
+     * The entity manager.
+     */
+    private final EntityManager entityManager;
+    /**
+     * Synchronization thread.
+     */
+    private final Thread thread;
+    /**
+     * Shutdown requested.
+     */
+    private boolean shutdown = false;
+
+    /**
+     * Constructor which starts synchronizer.
+     *
+     * @param entityManager the entity manager.
+     */
+
+    public HootSynchronizer(final EntityManager entityManager) {
+        this.entityManager = entityManager;
+
+        final long synchronizePeriodMillis = Long.parseLong(PropertiesUtil.getProperty("translation-site",
+                "synchronize-period-millis"));
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long lastTimeMillis = System.currentTimeMillis();
+                lastTimeMillis = lastTimeMillis - lastTimeMillis % synchronizePeriodMillis;
+
+                while (!shutdown) {
+
+                    while (System.currentTimeMillis() < lastTimeMillis + synchronizePeriodMillis) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (final InterruptedException e) {
+                            LOGGER.debug(e);
+                            if (shutdown) {
+                                return;
+                            }
+                        }
+                    }
+
+                    lastTimeMillis = System.currentTimeMillis();
+                    lastTimeMillis = lastTimeMillis - lastTimeMillis % synchronizePeriodMillis;
+                    synchronize();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Synchronizes bundles and database.
+     */
+    private void synchronize() {
+        entityManager.clear();
+
+        executeShellCommand(PropertiesUtil.getProperty("translation-site", "pre-synchronize-command-hook"));
+
+        final String bundleCharacterSet = PropertiesUtil.getProperty("translation-site", "bundle-character-set");
+        final String[] prefixes = PropertiesUtil.getProperty("translation-site", "bundle-path-prefixes").split(",");
+
+        for (final String prefixPart : prefixes) {
+            final String prefix = prefixPart.split(":")[1];
+            final String host = prefixPart.split(":")[0];
+            final Company company = CompanyDao.getCompany(entityManager, host);
+            final File baseBundle = new File(prefix + ".properties");
+            if (!baseBundle.exists()) {
+                LOGGER.info("Base bundle does not exist: " + baseBundle.getAbsolutePath());
+                continue;
+            }
+
+            LOGGER.info("Base bundle exists: " + baseBundle.getAbsolutePath());
+            String baseName = baseBundle.getName().substring(0, baseBundle.getName().length() - 11);
+            if (baseName.indexOf('_') >= 0) {
+                baseName = baseName.substring(0, baseName.indexOf('_'));
+            }
+
+            final File bundleDirectory = baseBundle.getParentFile();
+            final String bundleDirectoryPath = bundleDirectory.getAbsolutePath();
+
+            LOGGER.info("Basename: " + baseName);
+            LOGGER.info("Path: " + bundleDirectoryPath);
+
+            final Set<Object> keys;
+            final Properties baseBundleProperties;
+            try {
+                baseBundleProperties = new Properties();
+                final FileInputStream baseBundleInputStream = new FileInputStream(baseBundle);
+                baseBundleProperties.load(new InputStreamReader(baseBundleInputStream, bundleCharacterSet));
+                keys = baseBundleProperties.keySet();
+                baseBundleInputStream.close();
+            } catch (Exception e) {
+                LOGGER.error("Error reading bundle: " + baseName, e);
+                continue;
+            }
+
+            final Map<String, List<String>> missingKeys = new HashMap<String, List<String>>();
+            final SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+
+            for (final File candidate : bundleDirectory.listFiles()) {
+                if (candidate.getName().startsWith(baseName) && candidate.getName().endsWith(".properties")) {
+
+                    final String name = candidate.getName().split("\\.")[0];
+                    final String[] parts = name.split("_");
+
+                    String candidateBaseName = parts[0];
+                    if (candidateBaseName.equals(baseName)) {
+                        String language = "";
+                        String country = "";
+                        if (parts.length > 1) {
+                            language = parts [1];
+                            if (parts.length > 2) {
+                                country = parts[2];
+                            }
+                        }
+
+                        LOGGER.info("Bundle basename: '" + candidateBaseName
+                                + "' language: '" + language + "' country: '" + country + "'");
+
+                        entityManager.getTransaction().begin();
+                        try {
+                            final Properties properties = new Properties();
+                            final FileInputStream bundleInputStream = new FileInputStream(candidate);
+                            properties.load(new InputStreamReader(bundleInputStream, bundleCharacterSet));
+                            bundleInputStream.close();
+
+                            final TypedQuery<Entry> query = entityManager.createQuery("select e from Entry as e where " +
+                                    "e.path=:path and e.basename=:basename and " +
+                                    "e.language=:language and e.country=:country order by e.key", Entry.class);
+                            query.setParameter("path", bundleDirectoryPath);
+                            query.setParameter("basename", baseName);
+                            query.setParameter("language", language);
+                            query.setParameter("country", country);
+                            final List<Entry> entries = query.getResultList();
+                            final Set<String> existingKeys = new HashSet<String>();
+
+                            for (final Entry entry : entries) {
+                                if (keys.contains(entry.getKey())) {
+                                    if (candidate.equals(baseBundle) || (entry.getValue().length() == 0 && properties.containsKey(entry.getKey()) &&
+                                            ((String) properties.get(entry.getKey())).length() > 0)) {
+                                        entry.setValue((String) properties.get(entry.getKey()));
+                                        entityManager.persist(entry);
+                                    }
+
+                                }
+                                existingKeys.add(entry.getKey());
+                            }
+
+                            for (final Object obj : keys) {
+                                final String key = (String) obj;
+
+                                final String value;
+                                if (properties.containsKey(key)) {
+                                    value = (String) properties.get(key);
+                                } else {
+                                    value = "";
+                                }
+
+                                if (!existingKeys.contains(key)) {
+                                    final Entry entry = new Entry();
+                                    entry.setOwner(company);
+                                    entry.setPath(bundleDirectoryPath);
+                                    entry.setBasename(baseName);
+                                    entry.setLanguage(language);
+                                    entry.setCountry(country);
+                                    entry.setKey(key);
+                                    entry.setValue(value);
+                                    entry.setCreated(new Date());
+                                    entry.setModified(entry.getCreated());
+                                    entityManager.persist(entry);
+
+                                    final String locale = entry.getLanguage() + "_" + entry.getCountry();
+
+                                    if (!missingKeys.containsKey(locale)) {
+                                        missingKeys.put(locale, new ArrayList<String>());
+                                    }
+
+                                    missingKeys.get(locale).add(entry.getKey());
+                                }
+
+                            }
+                            entityManager.getTransaction().commit();
+
+                            if (!candidate.equals(baseBundle)) {
+
+                                final FileOutputStream fileOutputStream = new FileOutputStream(candidate, false);
+                                final OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream,
+                                        bundleCharacterSet);
+                                final PrintWriter printWriter = new PrintWriter(writer);
+
+                                for (final Entry entry : query.getResultList()) {
+                                    printWriter.print("# Modified: ");
+                                    printWriter.print(format.format(entry.getModified()));
+                                    if (entry.getAuthor() != null) {
+                                        printWriter.print(" Author: ");
+                                        printWriter.print(entry.getAuthor());
+                                    }
+                                    printWriter.println();
+                                    printWriter.print(entry.getKey());
+                                    printWriter.print("=");
+                                    final String value = entry.getValue().replace("\n", "\\\n");
+                                    printWriter.println(value);
+                                }
+
+                                printWriter.flush();
+                                printWriter.close();
+                                fileOutputStream.close();
+
+
+                            }
+                        } catch (Exception e) {
+                            if (entityManager.getTransaction().isActive()) {
+                                entityManager.getTransaction().rollback();
+                            }
+                            LOGGER.error("Error reading bundle: " + baseName, e);
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            final String smtpHost = PropertiesUtil.getProperty("translation-site", "smtp-host");
+            for (final String locale : missingKeys.keySet()) {
+                final List<String> keySet = missingKeys.get(locale);
+
+                final String subject = "Please translate " + locale;
+                String content = "Missing keys are: ";
+                for (final String key : keySet) {
+                    content += key + "\n";
+                }
+
+                final Group group = UserDao.getGroup(entityManager, company, locale);
+
+                if (group != null) {
+                    final List<User> users = UserDao.getGroupMembers(entityManager, company, group);
+                    for (final User user : users) {
+                        LOGGER.info("Sending translation request to " + user.getEmailAddress() + " for " + locale +
+                                " keys " + keySet);
+                        EmailUtil.send(smtpHost,
+                                user.getEmailAddress(), company.getSupportEmailAddress(), subject, content);
+                    }
+                }
+            }
+
+
+        }
+
+        executeShellCommand(PropertiesUtil.getProperty("translation-site", "post-synchronize-command-hook"));
+
+    }
+
+    /**
+     * Executes requested shell command.
+     *
+     * @param cmd the shell command to execute
+     */
+    private void executeShellCommand(final String cmd) {
+        LOGGER.debug("Executing shell command: " + cmd);
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec(new String[] {"/bin/sh", "-c", cmd});
+            process.waitFor();
+
+            String line;
+
+            BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            while ((line = error.readLine()) != null){
+                LOGGER.error(line);
+            }
+            error.close();
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line = input.readLine()) != null){
+                LOGGER.info(line);
+            }
+
+            input.close();
+
+            LOGGER.debug("Executed shell command: " + cmd);
+        } catch (final Throwable t) {
+            LOGGER.error("Error executing shell command: " + cmd, t);
+        }
+    }
+
+    /**
+     * Shutdown.
+     */
+    public final void shutdown() {
+        shutdown = true;
+        try {
+            thread.interrupt();
+            thread.join();
+        } catch (final InterruptedException e) {
+            LOGGER.debug(e);
+        }
+    }
+
+}
